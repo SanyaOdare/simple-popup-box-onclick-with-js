ed5ed094b4b6184816e8123c9a3211f81adb9420
Sanya Oluwadare <sanyaodare@gmail.com>
2021-12-18 20:56:37 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// function to open popup
+function openBox() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function closeBox() {
+  document.getElementById("popup").style.display = "none";
+}
